f0ec546db03fe07eb772c9a8f4154bb47f69a6ca
ViktorLumiste <viktor.lumiste@gmail.com>
2023-05-02 13:07:41 +0300

    Projektid katsetamiseks

--- controllers/parcelmachines.ts
+++ controllers/parcelmachines.ts
@@ -3,11 +3,26 @@ import axios from "axios";
 
 const router: Router = Router();
 
-router.get("/parcelmachines", async (req: Request, res: Response) => {
+router.get("/nord-pool-price", async (req: Request, res: Response) => {
     const response = await axios.get(
-        "https://www.omniva.ee/locations.json"
+        "https://dashboard.elering.ee/api/nps/price?start="+req.query.start+"&end="+req.query.end
     );
-    res.json(response.data);
+    switch(req.params.country) {
+        case "ee":
+            res.json(response.data.data.ee);
+            break;
+        case "lv":
+            res.json(response.data.data.lv);
+            break;
+        case "lt":
+            res.json(response.data.data.lt);
+            break;
+        case "fi":
+            res.json(response.data.data.fi);
+            break;
+        default:
+            res.json([]);
+    }
 });
 
 export default router;
--- veebirakendus/src/App.js
+++ veebirakendus/src/App.js
@@ -1,66 +1,58 @@
-import { useEffect, useRef, useState } from 'react';
+import { useRef } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-    const [tooted, setTooted] = useState([]);
-    const idRef = useRef();
-    const nameRef = useRef();
-    const priceRef = useRef();
-    const isActiveRef = useRef();
-
+    const [prices, setPrices] = useState([]);
+    const [chosenCountry, setChosenCountry] = useState("ee");
+    const [start, setStart] = useState("");
+    const [end, setEnd] = useState("");
+    const startRef = useRef();
+    const endRef = useRef();
 
     useEffect(() => {
-        fetch("http://localhost:4444/tooted")
-            .then(res => res.json())
-            .then(json => setTooted(json));
-    }, []);
-
-    function kustuta(index) {
-        fetch("http://localhost:4444/tooted/kustuta/" + index, {"method": "DELETE"})
-            .then(res => res.json())
-            .then(json => setTooted(json));
-    }
-
-    ////////////////////////
-    function lisa() {
-        const uusToode = {
-            "id": idRef.current.value,
-            "name": nameRef.current.value,
-            "price": priceRef.current.value,
-            "isActive": isActiveRef.current.value
+        if (start !== "" && end !== "") {
+            fetch("http://localhost:3000/nord-pool-price?country=" + chosenCountry + "&start=" + start + "&end=" + end)
+                .then(res => res.json())
+                .then(json => {
+                    setPrices(json);
+                });
         }
-        fetch("http://localhost:4444/tooted/lisa", {"method": "POST", "body": JSON.stringify(uusToode)})
-            .then(res => res.json())
-            .then(json => setTooted(json));
-    }
-    ////////////////////////
+    }, [chosenCountry, start, end]);
 
-    function dollariteks() {
-        const kurss = 1.1;
-        fetch("http://localhost:4444/tooted/hind-dollaritesse/" + kurss, {"method": "PATCH"})
-            .then(res => res.json())
-            .then(json => setTooted(json));
+    function updateStart() {
+        const startIso = new Date(startRef.current.value).toISOString();
+        setStart(startIso);
+    }
+
+    function updateEnd() {
+        const endIso = new Date(endRef.current.value).toISOString();
+        setEnd(endIso);
     }
 
     return (
-        <div className="App">
-            <label>ID</label> <br />
-            <input ref={idRef} type="number" /> <br />
-            <label>Nimi</label> <br />
-            <input ref={nameRef} type="text" /> <br />
-            <label>Hind</label> <br />
-            <input ref={priceRef} type="number" /> <br />
-            <label>Aktiivne</label> <br />
-            <input ref={isActiveRef} type="checkbox" /> <br />
-            <button onClick={() => lisa()}>Lisa</button>
-            {tooted.map((toode, index) =>
-                <div>
-                    <div>{toode.id}</div>
-                    <div>{toode.name}</div>
-                    <div>{toode.price}</div>
-                    <button onClick={() => kustuta(index)}>x</button>
-                </div>)}
-            <button onClick={() => dollariteks()}>Muuda dollariteks</button>
+        <div>
+            <button onClick={() => setChosenCountry("fi")}>Soome</button>
+            <button onClick={() => setChosenCountry("ee")}>Eesti</button>
+            <button onClick={() => setChosenCountry("lv")}>Läti</button>
+            <button onClick={() => setChosenCountry("lt")}>Leedu</button>
+            <input ref={startRef} onChange={updateStart} type="datetime-local" />
+            <input ref={endRef} onChange={updateEnd} type="datetime-local" />
+            {prices.length > 0 &&
+                <table style={{marginLeft: "100px"}}>
+                    <thead>
+                    <th style={{border: "1px solid #ddd", padding: "12px", backgroundColor: "#04AA6D"}}>Ajatempel</th>
+                    <th style={{border: "1px solid #ddd", padding: "12px", backgroundColor: "#04AA6D"}}>Hind</th>
+                    </thead>
+                    <tbody>
+                    <td style={{position: "absolute", left: "30px"}}>{chosenCountry}</td>
+                    {prices.map(data =>
+                        <tr key={data.timestamp}>
+                            <td style={{border: "1px solid #ddd", padding: "8px"}}>{new Date(data.timestamp * 1000).toISOString()}</td>
+                            <td style={{border: "1px solid #ddd", padding: "8px"}}>{data.price}</td>
+                        </tr>)}
+                    </tbody>
+                </table>}
         </div>
     );
 }
